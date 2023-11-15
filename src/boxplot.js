9a542b7807b32c5ac53dc9187b9716ac19fc41ba
ArkaPal-uic <apal7@uic.edu>
2023-11-15 13:46:05 -0600

    feat: box plot (broken SVG)

---
+++ src/boxplot.js
@@ -0,0 +1,145 @@
+import React, { useEffect, useState } from 'react';
+import * as d3 from 'd3';
+
+export default function BoxPlot(props) {
+  const [boxPlotData, setBoxPlotData] = useState();
+
+  async function fetchBoxPlotData() {
+    try {
+      const response = await fetch('JSON/boxplot_data.json');
+      const data = await response.json();
+
+      data.forEach(d => {
+        const magnitudes = d.Magnitude;
+        d.q1 = d3.quantile(magnitudes, 0.25);
+        d.median = d3.quantile(magnitudes, 0.5);
+        d.q3 = d3.quantile(magnitudes, 0.75);
+      });
+
+      setBoxPlotData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchBoxPlotData();
+  }, []);
+
+  useEffect(() => {
+    if (boxPlotData) {
+      const svg = d3.select("#boxplot_chart")
+        .append("svg")
+        .attr("width", 700)
+        .attr("height", 420);
+
+      svg.selectAll("*").remove();
+
+      const xScale = d3.scaleBand()
+        .domain(boxPlotData.map(d => d.label))
+        .range([120, 680]);
+
+      const yScale = d3.scaleLinear()
+        .domain([0, d3.max(boxPlotData.flatMap(d => d.Magnitude))])
+        .range([300, 120]);
+
+      svg.selectAll(".box")
+        .data(boxPlotData)
+        .enter()
+        .append("rect")
+        .attr("class", "box")
+        .attr("x", d => xScale(d.label) - 10)
+        .attr("y", d => yScale(d.q3))
+        .attr("width", 20)
+        .attr("height", d => yScale(d.q1) - yScale(d.q3))
+        .attr("fill", "none")
+        .attr("stroke", "black")
+        .attr("stroke-width", 1);
+
+      svg.selectAll(".whisker")
+        .data(boxPlotData)
+        .enter()
+        .append("line")
+        .attr("class", "whisker")
+        .attr("x1", d => xScale(d.label))
+        .attr("y1", d => yScale(d3.min(d.Magnitude)))
+        .attr("x2", d => xScale(d.label))
+        .attr("y2", d => yScale(d3.max(d.Magnitude)))
+        .attr("stroke", "black")
+        .attr("stroke-width", 1);
+
+      svg.selectAll(".upperWhisker")
+        .data(boxPlotData)
+        .enter()
+        .append("line")
+        .attr("class", "upperWhisker")
+        .attr("x1", d => xScale(d.label) - 10)
+        .attr("y1", d => yScale(d3.max(d.Magnitude)))
+        .attr("x2", d => xScale(d.label) + 10)
+        .attr("y2", d => yScale(d3.max(d.Magnitude)))
+        .attr("stroke", "green")
+        .attr("stroke-width", 2);
+
+      svg.selectAll(".lowerWhisker")
+        .data(boxPlotData)
+        .enter()
+        .append("line")
+        .attr("class", "lowerWhisker")
+        .attr("x1", d => xScale(d.label) - 10)
+        .attr("y1", d => yScale(d3.min(d.Magnitude)))
+        .attr("x2", d => xScale(d.label) + 10)
+        .attr("y2", d => yScale(d3.min(d.Magnitude)))
+        .attr("stroke", "blue")
+        .attr("stroke-width", 2);
+
+      svg.selectAll(".median")
+        .data(boxPlotData)
+        .enter()
+        .append("line")
+        .attr("class", "median")
+        .attr("x1", d => xScale(d.label) - 5)
+        .attr("y1", d => yScale(d.median))
+        .attr("x2", d => xScale(d.label) + 5)
+        .attr("y2", d => yScale(d.median))
+        .attr("stroke", "purple")
+        .attr("stroke-width", 1);
+
+      svg.append("line")
+        .attr("x1", 100)
+        .attr("y1", 300)
+        .attr("x2", 700)
+        .attr("y2", 300)
+        .attr("stroke", "black")
+        .attr("stroke-width", 1);
+
+      const yAxis = d3.axisLeft(yScale).ticks(5);
+
+      svg.append("g")
+        .attr("transform", "translate(100, 0)")
+        .call(yAxis);
+
+      svg.selectAll(".x-label")
+        .data(boxPlotData)
+        .enter()
+        .append("text")
+        .attr("class", "x-label")
+        .text(d => d.label)
+        .attr("x", d => xScale(d.label))
+        .attr("y", 370)
+        .attr("text-anchor", "middle")
+        .style("font-size", "10px")
+        .attr("transform", d => `rotate(-90, ${xScale(d.label)}, 370)`);
+
+      svg.append("text")
+        .attr("x", 400)
+        .attr("y", 20)
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .text("Box Plot by Label");
+    }
+  }, [boxPlotData]);
+
+  return (
+    <div id='boxplot_chart'></div>
+  );
+}
