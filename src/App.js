99ff7b455a280cc58f2ca6d9870287ec629ca350
Chintan Dobariya <chintandobaria18@gmail.com>
2023-11-15 14:56:55 -0600

    added radar chart, removed unused code from app.js

--- src/App.js
+++ src/App.js
@@ -8,54 +8,13 @@ import './App.css';
 import Boxplot from './boxplot';
 import Violinplot from './violin_plot';
 import * as d3 from 'd3';
+import RadarChart from './RadarPlot';
 
 
 function App() {
 
   //state deciding if we are looking at the blackhat or whitehat visualization
   const [viewToggle, setViewToggle] = useState('whitehat');
-
-  //state for the data, since it loads asynchronously
-  const [map, setMap] = useState();
-  const [gunData, setGunData] = useState();
-
-  //we put some states (brushing, zooming)
-  //at the top level and pass setZoomedState etc to the map
-  //so we can do brushing accross multiple components
-  const [zoomedState,setZoomedState] = useState();
-  const [selectedStuff,setSelectedStuff] = useState();
-  const [brushedState,setBrushedState] = useState();
-
-  //filter for the linked view in whitehat stats
-  const [sortKey,setSortKey] = useState('age');
-
-  //load map contours
-  //react looks into the '/public' folder by default
-  // async function fetchMap(){
-  //   fetch('us-states.geojson').then(paths=>{
-  //     paths.json().then(data=>{
-  //       setMap(data);
-  //     })
-  //   })
-  // }
-
-  // //fetch gun data and attach a timestamp to make sorting dates easier for filters
-  // async function fetchGunData(){
-  //   fetch('processed_gundeaths_data.json').then(d => {
-  //     d.json().then(gd=>{
-  //       console.log('gundata',gd)
-  //       setGunData(gd);
-  //     })
-  //   })
-  // }
-
-
-  //fetch data, called only once
-  // useEffect(()=>{
-  //   fetchMap();
-  //   fetchGunData()
-  // },[])
-
  
   //called to draw the whitehat visualization
   function makeWhiteHat(){
@@ -65,16 +24,18 @@ function App() {
             <div style={{'width':'calc(100% - 2em)','height':'calc(100% - 2em)','display':'flex', 'flexDirection': 'column', 'marginLeft': '1.5em'}}>
               <div style={{'height': '100%','width':'calc(100% - 2em)', 'display':'flex', 'justifyContent':'space-between'}}>
                   <div style={{ 'height': '100%', 'width':'calc(100% - 25em)', 'border':'1px solid black'}}>
-                    <Boxplot>
-                    </Boxplot>
+                    <Boxplot />
                   </div>
-                  <div style={{ 'height': '100%', 'width':'calc(100% - 25em)', 'border':'1px solid black'}}> </div>
-                    <Violinplot>
-                    </Violinplot>
+                  <div style={{ 'height': '100%', 'width':'calc(100% - 25em)', 'border':'1px solid black'}}> 
+                    <Violinplot />
+                  </div>
+                    
                   <div style={{ 'height': '100%', 'width':'calc(100% - 25em)', 'border':'1px solid black'}}> </div>
               </div>
               <div style={{'height': '100%','width':'calc(100% - 2em)', 'display':'flex', 'justifyContent':'space-between'}}>
-                  <div style={{ 'height': '100%', 'width':'calc(100% - 25em)', 'border':'1px solid black'}}> </div>
+                  <div style={{ 'height': '100%', 'width':'calc(100% - 25em)', 'border':'1px solid black'}}>
+                    <RadarChart />
+                  </div>
                   <div style={{ 'height': '100%', 'width':'calc(100% - 25em)', 'border':'1px solid black'}}> </div>
                   <div style={{ 'height': '100%', 'width':'calc(100% - 25em)', 'border':'1px solid black'}}> </div>
               </div>
@@ -83,59 +44,12 @@ function App() {
         )
       }
 
-  //function for a simpler chloropleth map
-  // function makeBlackHat(){
-
-  //   return (
-  //     <>
-  //       <div style={{'width':'100%','height':'50%','display':'inline-block'}}>
-  //         <div 
-  //           style={{'height': '100%','width':'calc(100% - 15em)','display':'inline-block'}}
-  //         >
-  //             <Blackhat
-  //               map={map}
-  //               data={gunData}
-  //               ToolTip={ToolTip}
-  //               zoomedState={zoomedState}
-  //               setSelectedStuff={setSelectedStuff}
-  //               setZoomedState={setZoomedState}
-  //               brushedState={brushedState}
-  //               setBrushedState={setBrushedState}
-  //             />
-  //         </div>
-  //         <div 
-  //           className={'shadow'}
-  //           style={{'height': '100%','width':'14em','display':'inline-block','verticalAlign':'text-bottom'}}
-  //         >
-  //           <h1>{'Instructions'}</h1>
-  //           <p>{'Click on each state to zoom and unzoom'}</p>
-  //         </div>
-  //       </div>
-  //       <div style={{'height': '49%','width':'99%'}}>
-  //         <div className={'title'} 
-  //           style={{'height':'2em','width':'100%','fontWeight':'bold','fontFamily':'Georgia'}}
-  //         >
-  //           {'Gun Deaths'}
-  //         </div>
-  //         <div style={{'height': 'calc(100% - 2em)','width': '50%','maxWidth': '60em','marginLeft':'25%'}}>
-  //           <BlackHatStats
-  //             data={gunData}
-  //             ToolTip={ToolTip}
-  //           />     
-  //         </div>   
-  //       </div>
-  //     </>
-  //   )
-  // }
-
+  
   // //toggle which visualization we're looking at based on the "viewToggle" state
   const hat = ()=>{
     if(viewToggle === 'whitehat'){
       return makeWhiteHat();
     }
-    // else{
-    //   return makeBlackHat();
-    // }
   }
 
   return (
@@ -163,40 +77,5 @@ function App() {
   );
 }
 
-
-// class ToolTip {
-//   static moveTTip(tTip, tipX, tipY){
-//     var tipBBox = tTip.node().getBoundingClientRect();
-//     while(tipBBox.width + tipX > window.innerWidth){
-//         tipX = tipX - 10 ;
-//     }
-//     while(tipBBox.height + tipY > window.innerHeight){
-//         tipY = tipY - 10 ;
-//     }
-//     tTip.style('left', tipX + 'px')
-//         .style('top', tipY + 'px')
-//         .style('visibility', 'visible')
-//         .style('z-index', 1000);
-//   }
-
-//   static moveTTipEvent(tTip, event){
-//       var tipX = event.pageX + 30;
-//       var tipY = event.pageY -20;
-//       this.moveTTip(tTip,tipX,tipY);
-//   }
-
-
-//   static hideTTip(tTip){
-//       tTip.style('visibility', 'hidden')
-//   }
-
-//   static addTTipCanvas(tTip, className, width, height){
-//       tTip.selectAll('svg').selectAll('.'+className).remove();
-//       let canvas = tTip.append('svg').attr('class',className)
-//           .attr('height',height).attr('width',width)
-//           .style('background','white');
-//       return canvas
-//   }
-// }
 
 export default App;
---
+++ src/RadarPlot.js
@@ -0,0 +1,390 @@
+// refernce
+//   https://benclinkinbeard.com/d3tips/log-and-power-scales/#:~:text=experiment%20with%20it.-,Log%20scales,-Whereas%20power%20scales
+//   https://gist.github.com/nbremer/21746a9668ffdf6d8242
+//   https://stackoverflow.com/questions/74406811/d3-js-spider-radar-chart-multiple-scales-on-axes
+
+import React, { useEffect, useState } from 'react';
+import * as d3 from 'd3';
+
+
+export default function RadarChart3(props){
+  var radar_margin = { top: 10, right: 0, bottom: 40, left: 0 },
+  radar_width = 400,
+  radar_height = 300;
+  
+  const [dataset, setDataset] = useState(null);
+  const columns = ['Header_Length', 'Srate', 'rst_count', 'Radius', 'flow_duration', 'urg_count', 'Magnitude', 'syn_count'];
+  const attacks = ['DDoS-ICMP_Flood', 'DDoS-UDP_Flood'];
+
+  async function fetchRadarData(){
+    fetch('JSON/radar_data.json').then(d => {
+      d.json().then(gd=>{
+        console.log('radarData',gd)
+        setDataset(gd);
+      })
+    })
+  }
+  useEffect(() => {
+    fetchRadarData()
+  },[])
+
+  var radar_color = d3
+        .scaleOrdinal()
+        .range(["blue", "orange"]);
+
+  var options = {
+    w: radar_width,
+    h: radar_height,
+    margin: radar_margin,
+    maxValue: 0.5,
+    levels: 4,
+    roundStrokes: true,
+    color: radar_color,
+  };
+  var cfg = {
+    w: 400,				//Width of the circle
+    h: 320,				//Height of the circle
+    margin: { top: 10, right: 10, bottom: 40, left: 50 }, //The margins of the SVG
+    levels: 3,				//How many levels or inner circles should there be drawn
+    maxValue: 0, 			//What is the value that the biggest circle will represent
+    labelFactor: 1.25, 	//How much farther than the radius of the outer circle should the labels be placed
+    wrapWidth: 60, 		//The number of pixels after which a label needs to be given a new line
+    opacityArea: 0.5, 	//The opacity of the area of the blob
+    dotRadius: 4, 			//The size of the colored circles of each blog
+    opacityCircles: 0.1, 	//The opacity of the circles of each blob
+    strokeWidth: 2, 		//The width of the stroke around each blob
+    roundStrokes: false,	//If true the area and stroke will follow a round path (cardinal-closed)
+    color: d3.scaleOrdinal(d3.schemeCategory10)           //Color function
+  };
+
+  //Put all of the options into a variable called cfg
+  if ("undefined" !== typeof options) {
+    for (var i in options) {
+      if ("undefined" !== typeof options[i]) {
+        cfg[i] = options[i];
+      }
+    } //for i
+  } //if
+  var radius = Math.min(cfg.w/3, cfg.h/2.8); 	//Radius of the outermost circle
+  const scaleList = React.useMemo(() => ({  
+      "Header_Length": [0, 7500, 15000, 22500, 30000],//"Header_Length"
+      "Srate": [0, 5000, 10000, 15000, 20000],//"Srate"
+      "rst_count": [0, 375, 750, 1125, 1500],//"rst_count"
+      "Radius": [0, 200, 400, 600, 800],//"Radius"
+      "flow_duration": [0, 175, 350, 525, 700],//"flow_duration"
+      "urg_count": [0, 100, 200, 300, 400],//"urg_count"
+      "Magnitude": [0, 12, 25, 36, 50],//"Magnitude"
+      "syn_count": [0, 0.5, 1, 1.5, 2]//"syn_count"
+    }), [])
+
+  const rScaleList = React.useMemo(() => ({
+    "Header_Length": d3.scaleLinear().range([0, radius]).domain([0, scaleList['Header_Length'][4]]), // "Header_Length"
+    "Srate": d3.scaleLinear().range([0, radius]).domain([0, scaleList['Srate'][4]]), // "Srate"
+    "rst_count": d3.scaleLog().range([0, radius]).domain([0.001, scaleList['rst_count'][4]]), // "rst_count":
+    "Radius": d3.scaleLog().range([0, radius]).domain([0.001, scaleList['Radius'][4]]), // "Radius"
+    "flow_duration": d3.scaleLog().range([0, radius]).domain([0.001, scaleList['flow_duration'][4]]), // "flow_duration":
+    "urg_count": d3.scaleLog().range([0, radius]).domain([0.001, scaleList['urg_count'][4]]), // "urg_count"
+    "Magnitude": d3.scaleLog().range([0, radius]).domain([0.001, scaleList['Magnitude'][4]]), // "Magnitude"
+    "syn_count": d3.scaleLog().range([0, radius]).domain([0.0001, scaleList['syn_count'][4]]), // "syn_count"
+  }), []);
+
+  var legendData = attacks.map((attack, index) => ({
+    name: attack,
+    color: cfg.color(index),
+  }));
+
+  useEffect(() => {
+    if(dataset !== null) {
+        function transformData(attacks, dataset, columns, rScaleList) {
+          const result = [];
+          for (const i in attacks) {
+            const transformedArray = Object.entries(dataset[attacks[i]])
+              .filter(([axis, value]) => columns.includes(axis))
+              .map(([axis, value]) => ({ axis, value, scale: rScaleList[axis] }));
+            result.push(transformedArray);
+          }
+          return result;
+        }
+        
+        const data = transformData(attacks, dataset, columns, rScaleList);
+        console.log(data);
+      
+      var allAxis = (data[0].map(function(i, j){return i.axis}));	//Names of each axis
+      var total = allAxis.length;					//The number of different axes
+
+      var Format = d3.format('.1f');			 	//Percentage formatting
+      var angleSlice = (Math.PI * 2) / total;		//The width in radians of each "slice"
+
+
+
+
+      d3.select('#radar_chart3').select("svg").remove();
+      //Initiate the radar chart SVG
+      var rdr_svg = d3.select('#radar_chart3').append("svg");
+      rdr_svg
+        .attr("width",  400)
+        .attr("height", 320)
+        .attr("class", "radar" + '#radar_chart3');
+      //Append a g element		
+      var g = rdr_svg.append("g")
+        .attr("transform", "translate(" + (cfg.w/2 + cfg.margin.left) + "," + cfg.h/2  + ")");
+
+      
+      //Filter for the outside glow
+      var filter = g.append("defs").append("filter").attr("id", "glow"),
+      feGaussianBlur = filter
+        .append("feGaussianBlur")
+        .attr("stdDeviation", "1.8")
+        .attr("result", "coloredBlur"),
+      feMerge = filter.append("feMerge"),
+      feMergeNode_1 = feMerge.append("feMergeNode").attr("in", "coloredBlur"),
+      feMergeNode_2 = feMerge.append("feMergeNode").attr("in", "SourceGraphic");
+  
+      //Wrapper for the grid & axes
+      var axisGrid = g.append("g").attr("class", "axisWrapper");
+
+      //Draw the background circles
+      axisGrid.selectAll(".levels")
+        .data(d3.range(1,(cfg.levels+1)).reverse())
+        .enter()
+          .append("circle")
+          .attr("class", "gridCircle")
+          .attr("r", function(d, i){return (radius/cfg.levels)*d;})
+          .style("fill", "#CDCDCD")
+          .style("stroke", "#CDCDCD")
+          .style("fill-opacity", cfg.opacityCircles)
+          .style("filter" , "url(#glow)");
+
+          // var rScaleList = [
+          //   d3.scaleLinear().range([0, radius]).domain([0, scaleList[0][4]]), // "Header_Length"
+          //   d3.scaleLinear().range([0, radius]).domain([0, scaleList[1][4]]), // "Srate"
+          //   d3.scaleLog().range([0, radius]).domain([0.001, scaleList[2][4]]), // "rst_count":
+          //   d3.scaleLog().range([0, radius]).domain([0.001, scaleList[3][4]]), // "Radius"
+          //   d3.scaleLog().range([0, radius]).domain([0.001, scaleList[4][4]]), // "flow_duration":
+          //   d3.scaleLog().range([0, radius]).domain([0.001, scaleList[5][4]]), // "urg_count"
+          //   d3.scaleLog().range([0, radius]).domain([0.001, scaleList[6][4]]), // "Magnitude"
+          //   d3.scaleLog().range([0, radius]).domain([0.0001, scaleList[7][4]]), // "syn_count"
+          // ];
+          
+
+      //Create the straight lines radiating outward from the center
+      var axis = axisGrid.selectAll(".axis")
+        .data(allAxis)
+        .enter()
+          .append("g")
+          .attr("class", "axis");
+        
+      //scale
+      for (let echelleNumero = 0; echelleNumero < columns.length; echelleNumero++) {
+        axis
+          .append("text")
+          .attr("class", "textscale")
+          .style("font-size", "10px")
+          .attr("fill", "#0a0a0a")
+        //   .data(scaleList[echelleNumero])
+          .data(d3.range(0, 100+1, 25))
+          .attr("x", 4) // decale echelle  en abscisse
+          .attr("dy", "-8")
+          .attr("y", function (d, i) {
+            return (-radius * i) / 5;
+          }) // gere espacement entre données en y
+          .attr("transform", function (d, i) {
+            var angleI = (angleSlice * echelleNumero * 180) / Math.PI; // the angle to rotate the label
+            var flip = angleI < 90 || angleI > 270 ? false : true; // 180 if label needs to be flipped
+            if (flip == true) {
+              return "rotate(" + angleI + ")";
+            } else {
+              return "rotate(" + angleI + ")";
+            }
+          })
+          .text(function (d) { 
+            if (echelleNumero == 0) {
+                return d + ' %';
+              } else {
+                if (d != 0) {
+                  return d + ' %';
+                } else {
+                  return;
+                }
+              }
+          });
+      }
+      // append the line
+      axis.append("line")
+        .attr("x1", 0)
+        .attr("y1", 0)
+        .attr("x2", function(d, i){ return radius * Math.cos(angleSlice*i - Math.PI/2); })
+        .attr("y2", function(d, i){ return radius * Math.sin(angleSlice*i - Math.PI/2); })
+        .attr("class", "line")
+        .style("stroke", "white")
+        .style("stroke-width", "2px");
+
+      //Append the labels at each axis
+      axis
+        .append("text")
+        .attr("class", "legend")
+        .style("font-size", "11px")
+        .attr("text-anchor", "middle")
+        .attr("fill", "black")
+        .attr("dy", "1px")
+        .attr("x", function (d, i) {
+          return radius * 1.3 * Math.cos(angleSlice * i - Math.PI / 2);
+        })
+        .attr("y", function (d, i) {
+          return radius * 1.25 * Math.sin(angleSlice * i - Math.PI / 2);
+        })
+        .text(function (d) {
+          return d;
+        })
+        .call(wrap, cfg.wrapWidth);
+
+
+      //The radial line function
+      var radarLine = d3.lineRadial()
+        .curve(d3.curveLinearClosed)
+        .radius(function(d, i) { 
+          return d.scale(d.value); 
+        })
+        .angle(function(d, i) { return i * angleSlice; });
+		
+      if(cfg.roundStrokes) {
+        radarLine.curve(d3.curveLinearClosed);
+      }
+
+      //Create a wrapper for the blobs	
+      var blobWrapper = g.selectAll(".radarWrapper")
+        .data(data)
+        .enter()
+          .append("g")
+          .attr("class", "radarWrapper");
+      
+      //Append the backgrounds	
+      blobWrapper
+        .append("path")
+        .attr("class", "radarArea")
+        .attr("d", function(d,i) { return radarLine(d); })
+        .style("fill", function(d,i) { return cfg.color(i); })
+        .style("fill-opacity", cfg.opacityArea)
+        .on('mouseover', function (d,i){
+          //Dim all blobs
+          d3.selectAll(".radarArea")
+            .transition().duration(200)
+            .style("fill-opacity", 0.1); 
+          //Bring back the hovered over blob
+          d3.select(this)
+            .transition().duration(200)
+            .style("fill-opacity", 0.7);	
+        })
+        .on('mouseout', function(){
+          //Bring back all blobs
+          d3.selectAll(".radarArea")
+            .transition().duration(200)
+            .style("fill-opacity", cfg.opacityArea);
+        });
+
+      //Create the outlines	
+      blobWrapper.append("path")
+        .attr("class", "radarStroke")
+        .attr("d", function(d,i) { return radarLine(d); })
+        .style("stroke-width", cfg.strokeWidth + "px")
+        .style("stroke", function(d,i) { return cfg.color(i); })
+        .style("fill", "none")
+        .style("filter" , "url(#glow)");
+      
+      //Append the circles
+      blobWrapper.selectAll(".radarCircle")
+        .data(function(d,i) { return d; })
+        .enter().append("circle")
+        .attr("class", "radarCircle")
+        .attr("r", cfg.dotRadius)
+        .attr("cx", function(d,i){ 
+          let ggg = d.scale(d.value) * Math.cos(angleSlice*i - Math.PI/2);
+          return ggg;
+        })
+        .attr("cy", function(d,i){ return d.scale(d.value) * Math.sin(angleSlice*i - Math.PI/2); })
+        .style("fill", function(d,i,j) { return cfg.color(j); })
+        .style("fill-opacity", 0.8);
+
+      
+      //Wrapper for the invisible circles on top
+      var blobCircleWrapper = g.selectAll(".radarCircleWrapper")
+        .data(data)
+        .enter().append("g")
+        .attr("class", "radarCircleWrapper");
+
+
+      //Append a set of invisible circles on top for the mouseover pop-up
+      blobCircleWrapper.selectAll(".radarInvisibleCircle")
+        .data(function(d,i) { return d; })
+        .enter().append("circle")
+        .attr("class", "radarInvisibleCircle")
+        .attr("r", cfg.dotRadius*1.5)
+        .attr("cx", function(d,i){ return d.scale(d.value) * Math.cos(angleSlice*i - Math.PI/2); })
+        .attr("cy", function(d,i){ return d.scale(d.value) * Math.sin(angleSlice*i - Math.PI/2); })
+        .style("fill", "none")
+        .style("pointer-events", "all")
+        .on("mouseover", function(e, d) {
+          let newX = parseFloat(d3.select(this).attr('cx')) - 10;
+          let newY = parseFloat(d3.select(this).attr('cy')) - 10;
+          rdr_tooltip
+            .attr('x', newX)
+            .attr('y', newY)
+            .text(Format(d.value))
+            .transition().duration(200)
+            .style('opacity', 1);
+        })
+        .on("mouseout", function(){
+          rdr_tooltip.transition().duration(200).style("opacity", 0);
+        });
+      
+      //Set up the small tooltip for when you hover over a circle
+      var rdr_tooltip = g.append("text")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+      //Taken from http://bl.ocks.org/mbostock/7555321
+      //Wraps SVG text	
+      function wrap(text, width) {
+        text.each(function() {
+        var text = d3.select(this),
+          words = text.text().split(/\s+/).reverse(),
+          word,
+          line = [],
+          lineNumber = 0,
+          lineHeight = 1.4, // ems
+          y = text.attr("y"),
+          x = text.attr("x"),
+          dy = parseFloat(text.attr("dy")),
+          tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
+          
+        while ((word = words.pop())) {
+          line.push(word);
+          tspan.text(line.join(" "));
+          if (tspan.node().getComputedTextLength() > width) {
+            line.pop();
+            tspan.text(line.join(" "));
+            line = [word];
+            tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+          }
+        }
+        });
+      }//wrap	
+      
+
+
+
+    }
+
+
+  }, [dataset])
+
+
+  return (
+    <div>
+      <h3>Attribute contribution over attack</h3>
+      <div id="radar_chart3">
+
+      </div>
+    </div>
+  );
+};
